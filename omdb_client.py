e325ce45d395ac69b3ee7fc2fe10da36935445bd
DemigodKS <mankok222@gmail.com>
2025-05-03 08:31:14 +0300

    Update omdb_client.py

--- omdb_client.py
+++ omdb_client.py
@@ -18,22 +18,6 @@ class OmdbClient:
         return data1
 
 
-# async def main():
-#         gg = OmdbClient()
-#         var1 = await gg.search_movie(title = "Big")
-#         pprint(var1)
-# asyncio.run(main())
-
-# gg = OmdbClient()
-#         names = ['Big', 'Green book', 'Hangover']
-#         #id_number = ['tt0089560', 'tt0499549']
-#         list_name = [gg.search_movie(name) for name in names]
-#         #id_list = [gg.get_movie(id_n) for id_n in id_number]
-#         all_names = await asyncio.gather(*list_name)
-#         #all_id = await asyncio.gather(*id_list)
-#         print(all_names)
-#         #pprint(all_id, sort_dicts=False)
-# asyncio.run(main())
 
 
 
